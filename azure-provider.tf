d3a88777b18be3f463c197dc00012811bf28b33f
Stom1294 <snehamerlin7@gmail.com>
2024-02-06 17:17:33 -0500

    provider config

---
+++ azure-provider.tf
@@ -0,0 +1,22 @@
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.85.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  client_id = var.azure_client_id
+  client_secret = var.azure_client_secret
+  tenant_id = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
+
+  features {
+    
+  }
+}
